25a1d6df27b94b667b84aefc87c20ca3c021ce22
Weslley Ceresa <ceresaconsultoria@gmail.com>
2022-01-26 22:00:15 -0300

    Novos metodos

---
+++ src/Fabricante.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Fbits;
+
+use Fbits\Core\FbitsHttp;
+use Fbits\Enum\ReturnCode;
+use Fbits\Exceptions\FbitsException;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+
+/**
+ * Description of Fabricante
+ *
+ * @author weslley
+ */
+class Fabricante extends FbitsHttp{
+    
+    public function __construct() {
+        $controller = FbitsController::getInstance();
+        parent::__construct($controller->getConfig());
+    }
+            
+    public function listarFabricantes(){
+        $controller = FbitsController::getInstance();
+        
+        try{
+            $response = $this->http->get("/categorias", array(
+                "headers" => [
+                    "Authorization" => "BASIC " . $controller->getToken()
+                ]
+            ));
+            
+            $body = (string)$response->getBody();
+                        
+            return json_decode($body);
+            
+        } catch (ServerException $ex) {
+            $response = $ex->getResponse();
+            $body = (string)$response->getBody();
+            $json = json_decode($body);               
+                            
+            if(is_object($json)){
+                if(isset($json->codigo))
+                    throw new FbitsException(ReturnCode::codeDescription($json->codigo, $ex->getMessage()), $response->getStatusCode());
+            }                          
+            
+            throw new FbitsException($ex->getMessage(), $response->getStatusCode());
+        } catch (ClientException $ex) {
+            $response = $ex->getResponse();
+            $body = (string)$response->getBody();
+            $json = json_decode($body);               
+                                    
+            if(is_object($json)){
+                if(isset($json->codigo))
+                    throw new FbitsException(ReturnCode::codeDescription($json->codigo, $json->mensagem), $response->getStatusCode());
+            }                          
+            
+            throw new FbitsException($ex->getMessage(), $response->getStatusCode());
+        }
+    }
+}
